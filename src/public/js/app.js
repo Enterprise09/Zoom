d89bf548afef1b21374c892468777b68e6c655cf
Enterprise09 <goldenstaytes@gmail.com>
2021-12-24 17:22:08 +0900

    create wss server on http server

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,1 @@
-alert("hi there");
+const socket = new WebSocket(`ws://${window.location.host}`);
--- src/server.js
+++ src/server.js
@@ -1,16 +1,25 @@
 import express from "express";
+import WebSocket from "ws";
+import http from "http";
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => {
   console.log("Listening on http://localhost:3000");
 };
-app.listen(3000, handleListen);
 
-app.get("/", (req, res) => {
-  res.render("home");
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (socket) => {
+  console.log(socket);
 });
+
+server.listen(3000, handleListen);
