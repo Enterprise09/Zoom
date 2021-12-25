e776834e23fc8be837067f99e19ac1e2397f3010
Enterprise09 <goldenstaytes@gmail.com>
2021-12-25 23:47:50 +0900

    [-] clean code for socket.io

--- src/server.js
+++ src/server.js
@@ -1,8 +1,6 @@
 import express from "express";
-import WebSocket from "ws";
+import SocketIo from "socket.io";
 import http from "http";
-
-const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
@@ -10,34 +8,16 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
+const app = express();
+const httpServer = http.createServer(app);
+const io = SocketIo(httpServer);
+
+io.on("connection", (socket) => {
+  console.log(socket);
+});
+
 const handleListen = () => {
   console.log("Listening on http://localhost:3000");
 };
 
-const server = http.createServer(app);
-
-const wss = new WebSocket.Server({ server });
-
-wss.on("connection", (socket) => {
-  console.log("Connected to Browser");
-  socket["nickname"] = "Anknown";
-  sockets.push(socket);
-  socket.on("close", () => console.log("Disconnect from the Browser"));
-  socket.on("message", (msg) => {
-    const message = JSON.parse(msg.toString());
-    switch (message.type) {
-      case "new_message":
-        sockets.forEach((aSocket) =>
-          aSocket.send(`${socket.nickname} : ${message.payload}`)
-        );
-        break;
-      case "nickname":
-        socket["nickname"] = message.payload;
-        break;
-    }
-  });
-});
-
-const sockets = [];
-
-server.listen(3000, handleListen);
+httpServer.listen(3000, handleListen);
