f18cd6a8c3e59943f9de2539824e5bc94a2c874b
Enterprise09 <goldenstaytes@gmail.com>
2021-12-26 21:13:38 +0900

    [+] make or enter the room

--- src/server.js
+++ src/server.js
@@ -2,18 +2,25 @@ import express from "express";
 import SocketIo from "socket.io";
 import http from "http";
 
+const app = express();
+const httpServer = http.createServer(app);
+const io = SocketIo(httpServer);
+
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
-const app = express();
-const httpServer = http.createServer(app);
-const io = SocketIo(httpServer);
-
 io.on("connection", (socket) => {
+  socket.onAny((event) => {
+    console.log(`Socket Event: ${event}`);
+  });
   console.log(socket);
+  socket.on("enter_room", (roomName, done) => {
+    socket.join(roomName);
+    done();
+  });
 });
 
 const handleListen = () => {
