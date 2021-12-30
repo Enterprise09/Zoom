baed589d2f527f0727674f5639589e8907860fcf
Enterprise09 <goldenstaytes@gmail.com>
2021-12-30 13:33:15 +0900

    [-] clear code for video call

--- src/server.js
+++ src/server.js
@@ -19,57 +19,6 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
-function publicRooms() {
-  const {
-    sockets: {
-      adapter: { sids, rooms },
-    },
-  } = io;
-  const publicRooms = [];
-  console.log(rooms);
-  rooms.forEach((_, key) => {
-    if (sids.get(key) === undefined) {
-      publicRooms.push(key);
-    }
-  });
-  return publicRooms;
-}
-
-function countRoom(roomName) {
-  return io.sockets.adapter.rooms.get(roomName)?.size;
-}
-
-io.on("connection", (socket) => {
-  socket["nickname"] = "Anknown";
-  socket.onAny((event) => {
-    console.log(socket.rooms);
-    console.log(`Socket Event: ${event}`);
-  });
-  // console.log(socket);
-  socket.on("enter_room", (roomName, nickname, done) => {
-    socket["nickname"] = nickname;
-    socket.join(roomName);
-    done();
-    socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName)); //왜 방을 만들 때는 실행이 안되는지?
-    io.sockets.emit("room_change", publicRooms());
-  });
-
-  socket.on("disconnecting", () => {
-    socket.rooms.forEach((room) =>
-      socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1)
-    );
-  });
-
-  socket.on("disconnect", () => {
-    io.sockets.emit("room_change", publicRooms());
-  });
-
-  socket.on("new_message", (msg, room, done) => {
-    socket.to(room).emit("new_message", `${socket.nickname} : ${msg}`);
-    done();
-  });
-});
-
 const handleListen = () => {
   console.log("Listening on http://localhost:3000");
 };
