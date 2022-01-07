a25b446fbe77ab292be9e0d211c83d0f0d812a86
Enterprise09 <goldenstaytes@gmail.com>
2022-01-07 21:33:05 +0900

    [+] install firebase

--- src/server.js
+++ src/server.js
@@ -2,12 +2,13 @@ import express from "express";
 import { Server } from "socket.io";
 import { instrument } from "@socket.io/admin-ui";
 import http from "http";
+import cors from "cors";
 
 const app = express();
 const httpServer = http.createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: ["https://admin.socket.io"],
+    origin: ["https://admin.socket.io", "http://localhost:3000"],
     credentials: true,
   },
 });
@@ -20,13 +21,15 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => {
-  console.log("Listening on http://localhost:3000");
+  console.log("Listening on http://localhost:3002");
 };
 
 io.on("connection", (socket) => {
   socket.on("join_room", (roomName) => {
     socket.join(roomName);
     socket.to(roomName).emit("welcome");
+    console.log(`Join Room Event: ${roomName}`);
+    console.log(io.sockets.adapter.sids);
   });
   socket.on("offer", (offer, roomName) => {
     socket.to(roomName).emit("offer", offer);
@@ -37,6 +40,12 @@ io.on("connection", (socket) => {
   socket.on("ice", (ice, roomName) => {
     socket.to(roomName).emit("ice", ice);
   });
+  socket.on("login", (email, pw) => {
+    socket.emit("successLogin", { email, pw });
+  });
+  socket.on("register", (email, pw) => {
+    socket.emit("successRegister", { email, pw });
+  });
 });
 
-httpServer.listen(3000, handleListen);
+httpServer.listen(3002, handleListen);
