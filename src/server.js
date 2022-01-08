9662fc78b71b6b970aaa1d87a242455333af8e0b
Enterprise09 <goldenstaytes@gmail.com>
2022-01-09 02:25:30 +0900

    [+] login & register with firebase auth

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import express from "express";
+import bodyPaser from "body-parser";
 import { Server } from "socket.io";
 import { instrument } from "@socket.io/admin-ui";
 import http from "http";
-import cors from "cors";
+import { authService } from "./db/firebaseService";
+import cros from "cors";
 
 const app = express();
 const httpServer = http.createServer(app);
@@ -16,9 +18,71 @@ instrument(io, { auth: false });
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
+app.use(
+  cros({
+    credentials: true,
+    origin: "http://localhost:3000",
+  })
+);
 app.use("/public", express.static(__dirname + "/public"));
+app.use(bodyPaser.json());
+app.use(bodyPaser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+
+app.post("/test", (req, res) => console.log(req.query));
+app.post("/login", async (req, res) => {
+  const {
+    body: { email, pw },
+  } = req;
+  try {
+    await authService
+      .signInWithEmailAndPassword(email, pw)
+      .then((user) => {
+        const {
+          user: { uid, email, displayName },
+        } = user;
+        console.log(`Sign In : ${uid}, ${email}, ${displayName}`);
+        res.send({
+          status: 200,
+        });
+      })
+      .catch((err) => {
+        console.log(`Sign In Error : ${err}`);
+        res.send({
+          status: 500,
+        });
+      });
+  } catch (err) {
+    console.log(err);
+  }
+});
+app.post("/register", async (req, res) => {
+  const {
+    body: { email, pw },
+  } = req;
+  try {
+    await authService
+      .createUserWithEmailAndPassword(email, pw)
+      .then((user) => {
+        const {
+          user: { uid, email, displayName },
+        } = user;
+        console.log(`Register : ${uid}, ${email}, ${displayName}`);
+        res.send({
+          status: 200,
+        });
+      })
+      .catch((err) => {
+        console.log(`Register Error : ${err}`);
+        res.send({
+          status: 500,
+        });
+      });
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 const handleListen = () => {
   console.log("Listening on http://localhost:3002");
@@ -40,12 +104,9 @@ io.on("connection", (socket) => {
   socket.on("ice", (ice, roomName) => {
     socket.to(roomName).emit("ice", ice);
   });
-  socket.on("login", (email, pw) => {
-    socket.emit("successLogin", { email, pw });
-  });
-  socket.on("register", (email, pw) => {
-    socket.emit("successRegister", { email, pw });
-  });
+
+  // Socket Test
+  socket.on("test", () => console.log("Test successful"));
 });
 
 httpServer.listen(3002, handleListen);
